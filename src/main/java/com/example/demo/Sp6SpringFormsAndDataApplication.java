d668b349f7fabc000c62b667b761c0b319b40e38
Rikimo23 <162639270+Rikimo23@users.noreply.github.com>
2024-08-16 14:48:43 -0400

    finished

---
+++ src/main/java/com/example/demo/Sp6SpringFormsAndDataApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Sp6SpringFormsAndDataApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Sp6SpringFormsAndDataApplication.class, args);
+	}
+
+}
